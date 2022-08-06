b57684f6411029183d5c110710d5840df6ffced9
Alonzo Machiraju <machthree@gmail.com>
2022-08-06 14:59:24 -0700

    spinning script separated and fixed

--- Assets/Scripts/PickupGun.cs
+++ Assets/Scripts/PickupGun.cs
@@ -7,12 +7,6 @@ public class PickupGun : MonoBehaviour
     [SerializeField]
     GameObject gun;
 
-    void Update()
-    {
-        Vector3 nRot = new Vector3(0, 10 * Time.deltaTime, 0);
-        transform.eulerAngles = nRot;
-    }
-
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Gun Picked Up");
